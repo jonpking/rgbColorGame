abd52b66cd3957455a2445f5359057c239ff8da9
Jonathan King <jonpking@gmail.com>
2020-05-09 21:34:44 -0700

    updated correct/wrong guesses to use a displayed text message on page instead of alerts, also wrong answers disappear from visible selections

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const colors = [
 const squares = document.querySelectorAll(".square");
 const pickedColor = colors[3];
 const colorDisplay = document.getElementById("colorDisplay");
+const messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,10 @@ for (var i = 0; i < squares.length; i++) {
         const clickedColor = this.style.backgroundColor;
         //compare color of clicked square to pickedColor
         if (clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
         } else {
-            alert("Wrong!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
 }
